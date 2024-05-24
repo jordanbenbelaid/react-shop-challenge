0cb11bfec08aa5e70c2071fd5a46ff33682a002f
Jordan <jordanbenbelaid@hotmail.com>
2024-05-24 15:10:41 +0100

    Challenge 2 added - make the buttons toggle

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -3,15 +3,47 @@ import '../CSS/ShopCard.css'
 
 const ShopCard = ({ name, price, imageUrl }) => {
 
+  let [isButtonPressed, setButtonName] = useState("Add to cart")
+
+  function newButtonName(){
+    setButtonName("In Cart")
+  }
+
+  function oldButtonName(){
+    setButtonName("Add to cart")
+  }
+
+  function change() {
+    if (isButtonPressed === "Add to cart") isButtonPressed = newButtonName();
+    else isButtonPressed = oldButtonName();
+}
+
   return (
     <div className="wrapper">
       <img src={imageUrl} width={200} height={250} alt={name} title={name} />
       <h3 className="name">{name}</h3>
       <p className="price">£{price.toFixed(2)}</p>
-      <button className="add-cart-button">Add to Cart</button>
+      <button 
+      onClick={change}
+      className="add-cart-button">
+        {isButtonPressed}
+      </button>
       
     </div>
   )
+
+
+// const [buttonClicked, setButton] = useState('Add to cart');
+ 
+//     const handleClick = () => {
+//         setButton(buttonClicked === 'Add to cart' ? 'In cart' : 'Add to cart');
+//     };
+ 
+//     return (
+//         <button onClick={handleClick}>
+//             {buttonClicked}
+//         </button>
+//     );
 }
 
 export default ShopCard;
