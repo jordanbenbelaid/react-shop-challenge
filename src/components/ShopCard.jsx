220ad54874c07c34da298451bf019c2649d833eb
Jordan <jordanbenbelaid@hotmail.com>
2024-05-24 14:13:32 +0100

    challenge 1 updated

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -3,26 +3,12 @@ import '../CSS/ShopCard.css'
 
 const ShopCard = ({ name, price, imageUrl }) => {
 
-  const [isButtonPressed, setButtonName] = useState(true)
-
-  function newButtonName(){
-    setButtonName("In Cart")
-  }
-
-  function resetButtonName(){
-    setButtonName("Add to cart")
-  }
-
   return (
     <div className="wrapper">
       <img src={imageUrl} width={200} height={250} alt={name} title={name} />
       <h3 className="name">{name}</h3>
       <p className="price">£{price.toFixed(2)}</p>
-      <button 
-      className="add-cart-button"
-      onClick={isButtonPressed ? newButtonName : resetButtonName}>
-        {isButtonPressed}
-      </button>
+      <button className="add-cart-button"></button>
       
     </div>
   )
